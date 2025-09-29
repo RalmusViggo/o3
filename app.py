d70f6bd7286ed452dcdc8f54d27cfc31e2efc79e
“RalmusViggo” <rasmus.lang_holmen@icloud.com>
2025-09-29 13:15:55 +0200

    jeg gjorde ting annerledes siden når routen er 'vis_brukere' så må den faktisk bli kalt på, som den ikke ble, så sa endret jeg det til '/brukere.html'

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/brukere.html')
+def vis_brukere():
+    return render_template("brukere.html", brukere = ["alice", "bob", "carol", "david", "eve"])
+
+
+if __name__ == "__main__":
+    app.run()
